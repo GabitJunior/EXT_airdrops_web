a1d322a774f21d1c18107292398e95c02687bb3c
GabitJunior <gabit.omarov@gmail.com>
2023-03-28 22:02:13 +0600

    Signed-off-by: GabitJunior <gabit.omarov@gmail.com>

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 // Подключаемся к контракту
-const contractAddress = "0x3c01b1d509E6A435770FfbC98F69E89F6DC22d7D"; //Замените вашим контрактом
+const contractAddress = "0x951749368582c91cfa73ad7c3fa7fca08a43e426"; //Замените вашим контрактом
 
 // Указываем ABI (Application Binary Interface) контракта
 const abi = [
-  {
-    inputs: [
-      {
-        internalType: "string",
-        name: "_note",
-        type: "string",
-      },
-    ],
-    name: "setNote",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getNote",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
+	{
+		"inputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "_contractAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address[]",
+				"name": "_airAddress",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "_amount",
+				"type": "uint256[]"
+			}
+		],
+		"name": "airdrop",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
 ];
+
 
 // Подключаемся к web3 провайдеру (метамаск)
 const provider = new ethers.providers.Web3Provider(window.ethereum, 97);
 
 let signer;
 let contract;
+
 
 //Запрашиваем аккаунты пользователя и подключаемся к первому аккаунту
 provider.send("eth_requestAccounts", []).then(() => {
